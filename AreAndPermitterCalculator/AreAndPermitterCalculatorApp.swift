6910dd7e3205ed208e7861f6de869c9fb3c7459b
griffin muir <gmuir@lcs.on.ca>
2023-01-19 11:05:11 -0500

    Initial Commit

---
+++ AreAndPermitterCalculator/AreAndPermitterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreAndPermitterCalculatorApp.swift
+//  AreAndPermitterCalculator
+//
+//  Created by Samantha Stewart on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct AreAndPermitterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
